fc0497aeb5d75a09ec33c2e7e7d398a7e9fca091
SantiagoCardenasR <30352306+SantiagoCardenasR@users.noreply.github.com>
2020-05-09 13:00:09 -0500

    cambio de colores en tabla
    Al cambiar valores se cambia el color de el select en la tabla

--- js/Vehiculos.js
+++ js/Vehiculos.js
@@ -1,38 +1,32 @@
+/* $(window).on("load resize ", function() 
+ {
+   var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+   $('.tbl-header').css({'padding-right':scrollWidth});
+ }).resize(); */
 
-      <script>
-          function colores(){
-              var color = document.getElementById("estado");
-              var estado = document.getElementById("estado").value;
-              if(estado=="Bueno"){
-                  color.className = "Bueno";
-              }
-              if(estado=="Malo"){
-                  color.className = "Malo";
-              }
-              if(estado=="Mantenimiento correctivo"){
-                  color.className = "Mantenimiento correctivo";
-              }
-              if(estado=="Mantenimiento Preventivo"){
-                color.className = "Mantenimiento Preventivo";
-            }
-          }
-      </script>
- 
-  <body>
-      
-              <option value="">Elegir Estado</option>
-              <option value="Aceptado">Aceptado</option>
-              <option value="Proceso">Proceso</option>
-              <option value="Cerrado">Cerrado</option>
-          </select>
-          <input type="submit" value="Enviar">
-      </form>
-  </body>
-  </html>
-  
-
-
-  $(window).on("load resize ", function() {
-    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-    $('.tbl-header').css({'padding-right':scrollWidth});
-  }).resize();
+function colores()
+{
+    var colors = document.getElementsByClassName("estado");
+    for( var i= 0; i < colors.length; i++)
+    {
+        var estado = colors[i].value;
+        if(estado=="Bueno")
+        {
+            colors[i].id = "Bueno";
+        }
+        if(estado=="Malo")
+        {
+            colors[i].id = "Malo";
+            
+        }
+        if(estado == "ManC")
+        {
+            colors[i].id = "ManC";
+            
+        }
+        if(estado=="MantenimientoPreventivo")
+        {
+            colors[i].id = "MantenimientoPreventivo";
+        }
+    }
+}
