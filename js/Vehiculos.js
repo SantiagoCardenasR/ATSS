fa3f0b07560aa6dc1e01d1a40631c230e041dfb8
SantiagoCardenasR <30352306+SantiagoCardenasR@users.noreply.github.com>
2020-05-12 12:03:51 -0500

    cambios finales formato Vehículo
    Cambios del formato de vehículo

--- js/Vehiculos.js
+++ js/Vehiculos.js
@@ -1,31 +1,32 @@
-
 function colores()
 {
-    var colors = document.getElementsByName("estado");
+    var colors = document.getElementsByName("estado_vehiculo");
     for( var i= 0; i < colors.length; i++)
     {
         var estado = colors[i].value;
+        var itemSemaforo = colors[i].parentElement.parentElement;
+        var botonSemaforo = itemSemaforo.getElementsByClassName("btn dropdown-toggle btn-light")[0];
         if(estado == "Estado")
         {
-            colors[i].id = "id_Estado";
+            botonSemaforo.id = "id_Estado";
         }
         if(estado=="Bueno")
         {
-            colors[i].id = "Bueno";
+            botonSemaforo.id = "Bueno";
         }
         if(estado=="Malo")
         {
-            colors[i].id = "Malo";
+            botonSemaforo.id = "Malo";
             
         }
         if(estado == "ManC")
         {
-            colors[i].id = "ManC";
+            botonSemaforo.id = "ManC";
             
         }
         if(estado=="MantenimientoPreventivo")
         {
-            colors[i].id = "MantenimientoPreventivo";
+            botonSemaforo.id = "MantenimientoPreventivo";
         }
     }
 }
